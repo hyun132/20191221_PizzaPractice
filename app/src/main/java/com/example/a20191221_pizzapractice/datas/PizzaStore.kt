186a2d470e556dc2b3ea53ef9165ec2d6c80e0b0
hyun <rlaalgus132@naver.com>
2019-12-21 15:35:03 +0900

    피자가게 근거 데이터 클래스 설정

---
+++ app/src/main/java/com/example/a20191221_pizzapractice/datas/PizzaStore.kt
@@ -0,0 +1,7 @@
+package com.example.a20191221_pizzapractice.datas
+
+class PizzaStore (storeName:String ,phoneNum:String, logoUrl:String){
+    var storeName = storeName
+    var phoneNum = phoneNum
+    var logoUrl = logoUrl
+}
